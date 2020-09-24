2dccdc0ef7144cd76ea5b241083a925c91d42ec7
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2020-09-23 21:13:15 -0300

    Main e Interface Consultavel

--- src/br/digital/com/desafio/Consultavel.kt
+++ src/br/digital/com/desafio/Consultavel.kt
@@ -1,5 +1,13 @@
 package br.digital.com.desafio
 
-interface Consultavel {
+import java.math.BigDecimal
 
+abstract class Consultavel {
+    var codigo: String? = null
+    var preco: BigDecimal? = null
+    var titulo: String? = null
+
+    override fun equals(other: Any?): Boolean {
+        return (other is Consultavel && other.codigo == this.codigo)
+    }
 }
